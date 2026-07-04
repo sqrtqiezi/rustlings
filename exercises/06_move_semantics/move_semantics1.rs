fcc912e124fbf2c29dd788dba560d64e6128e713
Niu Jin <njin.cool@gmail.com>
2026-07-04 21:59:02 +0800

    完成 06_move_semantics 练习与解答，更新运行状态
    更新 exercises/06_move_semantics 的多处题目修正与完善；同步更新 solutions/06_move_semantics 的参考解答；更新 .rustlings-state.txt 以记录当前练习进度

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,6 +1,6 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
@@ -19,6 +19,6 @@ mod tests {
     fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
         let vec1 = fill_vec(vec0);
-        assert_eq!(vec1, vec![22, 44, 66, 88]);
+        assert_eq!(*vec1, vec![22, 44, 66, 88]);
     }
 }
